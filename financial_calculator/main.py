c77ef460f0712b8517110c88001cbe244501feae
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-01-08 10:52:02 -0700

    Got ideo of what to do for financial calculator

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,1 +1,22 @@
 # Gabriel Crozier, Financial Calculator Main File
+
+
+def main():
+    pass
+
+
+# Possibly connect numbers together to use in multiple senerios
+
+# Savings Linear:
+# Need starting money
+# Need weekly / monethly, / other options for the person
+# Need add money to account and check how long they plan to save
+
+# Compound interest:
+# Start money
+# End Goal Time
+# Interest Value
+# How much per unit of time
+
+# budget calculator:
+# 
