a90b44d17f1695c67ee58bc60fc75d1e7952264c
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-01-09 10:52:42 -0700

    Added savings options inside of financial calculator

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,8 +1,48 @@
-# Gabriel Crozier, Financial Calculator Main File
+# Gabriel Crozier, Financial Calculator
 
+def savings():
+    print('\033c')
+    print('This calculator is designed to calculate when you will reach a savings goal.\n\
+This means you need to input the starting money amount, how frequently you deposit money, and how much you deposit each time.')
+    startMoney = input('How much money are you starting with? --->  ')
+    timeFrame = input("""How frequently are you depositing your money?
+Day, Week, Month, or Year (You can choose the specifics later)
+    --->  """)
+    timeFrame = input(f'How many {timeFrame.lower() + 's'} between each deposit? --->  ')
+    depositAmount = input('How much money are you depositing each time? --->  ')
+    
 
 def main():
-    pass
+    while True:
+        options = input("""
+What would you like to do?:
+1. Savings Calculator
+2. Compound Interest Calculator
+3. Budget Calculator
+4. Sale Price Calculator
+5. Tip Calculator
+Input the number corresponding to the option you would like to choose: 
+    --->  """)
+        if options.isdigit():
+            options = int(options)
+        else: options = 0
+        if options == 1:
+            savings()
+        elif options == 2:
+            pass
+        elif options == 3:
+            pass
+        elif options == 4:
+            pass
+        elif options == 5:
+            pass
+        else:
+            leaveQue = input('Your option was not valid, would you like to leave the calculator (y/n) --->  ')
+            if leaveQue == 'y':
+                break
+            elif leaveQue == '':
+                break
+
 
 
 # Possibly connect numbers together to use in multiple senerios
@@ -20,3 +60,4 @@ def main():
 
 # budget calculator:
 # 
+main()
