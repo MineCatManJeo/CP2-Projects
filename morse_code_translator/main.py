5e5dbab7723dcfecb174353c998e91c2c36acaa3
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-03 23:57:16 -0700

    Added morse code translator Added comments 100% not 3 minutes before due date

--- morse_code_translator/main.py
+++ morse_code_translator/main.py
@@ -1,6 +1,10 @@
 # Gabriel Crozier, Morse Code Translator
 
+
+# translates alphabet to morse code
 def abc_to_morse(abc, morse):
+    # turns text into a list, finds the corresponding morse code counterpart, translates it, and adds seperators when needed.
+
     print('\033cYou have chosen the alphabet to morse code translation. Input text to translate:')
     text_translate = list(input('  --->  ').lower())
 
@@ -10,10 +14,10 @@ def abc_to_morse(abc, morse):
             text_translated.append(morse[abc.index(text_translate[i])])
 
         elif text_translate[i] == ' ':
-            text_translated.append(' ')
+            text_translated.append('/')
     
         elif text_translate[i] == '.':
-            text_translated.append('\n')
+            text_translated.append(':')
 
         else:
             text_translated.append('?')
@@ -25,29 +29,28 @@ def abc_to_morse(abc, morse):
     print('\033c')
 
 
+# translates morese code to alphabet
+def morse_to_abc(abc, morse):
+    # splits into a list around the spaces, translates, adds in word / sentence seperators
 
-def morse_to_abc(abc, morse): # NOT DONE YET
     print('\033cYou have chosen the morse code to alphabet translation.')
-    print('(Type in each morse code character with . and _)')
-    print('Between each letter add a space and between words add a comma. Sentence')
-    morse_translate = list(input('  --->  ').lower())
+    print('(Type in each morse code character with . and -)')
+    print('Between each letter add a space and between words add a slash with spaces surrounding it ( / )')
+    print('Between sentences add a colon with spaces surrounding it ( : )')
+    morse_translate = input('  --->  ').split(' ')
 
-    morse_translated = ['  ']
+    morse_translated = '  '
     for i in range(len(morse_translate)):
-        if morse_translate[i] in abc:
-            morse_translated.append(morse[abc.index(morse_translate[i])])
-
-        elif morse_translate[i] == ' ':
-            morse_translated.append(' ')
-    
-        elif morse_translate[i] == '.':
-            morse_translated.append('\n')
-
-        else:
-            morse_translated.append('?')
+        if morse_translate[i] in morse:
+            morse_translated += abc[morse.index(morse_translate[i])]
+        elif morse_translate[i] == '/':
+            morse_translated += ' '
+        elif morse_translate[i] == ':':
+            morse_translated += '.'
+        else: morse_translated += '?'
         
-    print('\033cYour morse code has been translated!\n(? has replaced any non translatable symbols)\n(A new line means a new sentence): ')
-    print(' '.join(morse_translated))
+    print('\033cYour morse code has been translated!\n(? has replaced any non translatable symbols)\n')
+    print(morse_translated)
 
     input('\nHit enter if you are ready to move on ')
     print('\033c')
@@ -57,7 +60,7 @@ def morse_to_abc(abc, morse): # NOT DONE YET
 def main():
     # This is the list setup
     abc = list('abcdefghijklmnopqrstuvwxyz1234567890')
-    morse = '._ _... _._. _.. . .._. __. .... .. .___ _._ ._.. __ _. ___ .__. __._ ._. ... _ .._ ..._ .__ _.._ _.__ __.. .____ ..___ ...__ ...._ ..... _.... __... ___.. ____. _____'.split(' ')
+    morse = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. .---- ..--- ...-- ....- ..... -.... --... ---.. ----. -----'.split(' ')
     
     # This is main UI
     print('\033cWelcome to my morse code translator!\n')
@@ -79,9 +82,9 @@ def main():
                 
             elif option == '3':
                 break
-
+            # error handler for not correct number
             else: print(f'\033c|  {option}  | is not a valid number! Please try again!\n')
-
+        # error handler for not number
         else: print(f'\033c|  {option}  | is not a valid option! Please try again! (Options are the numbers 1, 2, 3)\n')
     print('\033cThank you for using this program!')
 main()
