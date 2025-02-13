d6a43c96e1104eb171661e9bbe467f30a4269bc6
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-13 10:51:37 -0700

    Changed personal library to work with new requirements Changes add items funciton Changed display function Need to make search and remove funcitons work

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,5 +1,10 @@
 # Gabriel Crozier, Personal Library
 
+def search_key(book):
+    return(book['book'])
+
+
+
 # This is useful when checking for a user input that is a number, just replace input with check_float and it will have error checking for numbers
 def check_float(input_text, default = 0):
     for i in range(10):
@@ -13,23 +18,48 @@ def check_float(input_text, default = 0):
         user_data = default
     return user_data
 
-# loops through each item in a set and displays it with a number
+
+
+# loops through each item in a list and displays it with a number
 def display(library):
+    # Sorts the library based alphebetically on the book title
+    library.sort(key=search_key)
+
+
+    # Lets user choose between a simple display or a more complicated display
+    while True:
+        option = input('Would you like to have minimum display or maximum display? (min, max) --->  ').lower()
+        if option in ['min','max']:
+            break
+        print(f'\033c| {option} | is not a valid option! Please try again: ')
+
     count = 0
     if len(library) == 0:
         print('There is nothing to display!')
     else:
         print('These are the books in your library: \n')
+    
+    # Loops through the library and displays it correlating with a number.
     for item in library:
         count += 1
-        print(f'{count}. {item}')
+        if option == 'min':
+            print(f'{count}. {item['book']} by {item['author']}')
+        if option == 'max':
+            print(f'{count}. {item['book']} by {item['author']}, {item['pages']} pages, Made in {item['year']}')
+
+
 
 # Combines author and book name in one string to help with sets
 def add_items(library):
     book = input('What is the name of the book would you like to add? --->  ')
     author = input(f'Who is the author of {book}? --->  ')
-    library.add(f'{book.title()} by {author.title()}')
+    pages = input(f'How many pages are in {book}? --->  ')
+    year = input(f'When was {book} made? --->  ')
+
+    library.append({'book':book.title(), 'author':author.title(), 'pages':pages, 'year':year})
     print(f'\033cYou just added {book.title()} by {author.title()}!')
+
+
 
 # REally similar to search function
 def remove_items(library):
@@ -41,11 +71,14 @@ def remove_items(library):
         if itemRemove.lower() in item.lower():
             count += 1
             foundItems.append(item)
+
     if count == 0: # Nothing found
         print('Couldn\'t find anything...')
+
     elif count == 1: # 1 thing found, seperate text to account for singularity
         print(f'The book {foundItems[0]} has been removed.')
         library.discard(foundItems[0])
+        
     else: # prints the list, then asks user which of the located results to remove
         print('Here\'s a list of what we found.')
         for i in range(1,count+1):
@@ -53,6 +86,8 @@ def remove_items(library):
         delete = int(check_float('Which book would you like to remove? (Number) --->  '))
         library.discard(foundItems[delete-1])
         print(f'{foundItems[delete-1]} has been removed.')
+
+
 
 def search(library): # same as the remove function, but it doesnt remove things 0:
     itemSearch = input('What book(s) would you like to look for? (Name or author) ---> ')
@@ -73,11 +108,14 @@ def search(library): # same as the remove function, but it doesnt remove things 
         print('Here\'s a list of what we found in your library.')
         for i in range(1,count+1):
             print(f'{i}. {foundItems[i-1]}')
+
+
+
 # Contains my UI and options menu
 def main():
     welcome_text = '\033cWelcome to your personal library of books!\n\n'
     # All items are added to this set which can be converted later to help with search function
-    library = set()
+    library = []
     while True:
         options = check_float(welcome_text + """What would you like to do?
 1. Display Contents
