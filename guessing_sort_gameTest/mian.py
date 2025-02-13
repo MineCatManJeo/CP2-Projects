8b2c5818a8e846278f48bf3c9ec24087d68c1382
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-13 10:52:36 -0700

    Added game fux later

---
+++ guessing_sort_gameTest/mian.py
@@ -0,0 +1,16 @@
+def sort_key(item):
+	
+	return(item)
+
+def user_word():
+	sentence = []
+    info = int(input('How many sentences would you like to write?'))
+    for i in range(info)
+        word = input("Write a sentence that could help you figure out my sorting algorithm")
+        sentence.append(word)
+
+def main():
+	print('Welcome to my sorting guessing game! You just need to guess how I sorted your lists!')
+	user_word()
+	
+main()
