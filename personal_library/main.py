4aae7813c935026f3a8ceab9d045ab3acc346ba7
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-14 15:06:26 -0700

    Significantly decreased the length of my remove function Began the change function

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,6 +1,6 @@
 # Gabriel Crozier, Personal Library
 
-def search_key(book):
+def sort_key(book):
     return(book['book'])
 
 
@@ -23,7 +23,7 @@ def check_float(input_text, default = 0):
 # loops through each item in a list and displays it with a number
 def display(library):
     # Sorts the library based alphebetically on the book title
-    library.sort(key=search_key)
+    library.sort(key=sort_key)
 
 
     if len(library) == 0:
@@ -64,50 +64,56 @@ def add_items(library):
 
 # REally similar to search function
 def remove_items(library):
-    itemRemove = input('What book would you like to remove? (Name or Author) ---> ')
+    book = search(library)
+    if book == False:
+        return
+    elif len(book) == 1:
+        library.remove(book[0])
+        print(f"{book[0]['book']} by {book[0]['author']} has been removed.")
+    else:
+        while True:
+            delete = int(check_float('Which book would you like to remove? (Number) --->  '))
+            if delete >= len(book):
+                print('That was too large of a number, please try again.')
+                continue
+            break
+        # Removes item chosen
+        library.remove(book[delete-1])
+        print(f"{book[delete-1]['book']} by {book[delete-1]['author']} has been removed.")
+
+    
+
+def search(library): # searches for an item and returns a list of valid items or just one item
+    itemSearch = input('What book would you like to look for? (Name or Author) ---> ')
     foundItems = []
     count = 0
     # Checks if your input is inside of a book in the library
     for item in library:
-        if itemRemove.lower() in ''.join(list(item.values())).lower():
+        if itemSearch.lower() in ''.join(list(item.values())).lower():
             count += 1
             foundItems.append(item)
 
-    if count == 0: # Nothing found
+    if count == 0: # Displays if nothing was found in library
         print('Couldn\'t find anything...')
+        return False
 
-    elif count == 1: # 1 thing found, seperate text to account for singularity
-        print(f'The book {foundItems[0]['book']} by {foundItems[0]['author']} has been removed.')
-        library.remove(foundItems[0])
-        
-    else: # prints the list, then asks user which of the located results to remove
-        print('Here\'s a list of what we found.')
-        for i in range(1,count+1):
-            print(f'{i}. {foundItems[i-1]['book']} by {foundItems[i-1]['author']}')
-        delete = int(check_float('Which book would you like to remove? (Number) --->  '))
-        library.remove(foundItems[delete-1])
-        print(f'{foundItems[delete-1]['book']} by {foundItems[delete-1]['author']} has been removed.')
+    elif count == 1: # Displays the item
+        print(f"The book {foundItems[0]['book']} by {foundItems[0]['author']} has been found in your library.")
+        return [foundItems[0]]
 
-
-
-def search(library): # same as the remove function, but it doesnt remove things 0:
-    itemSearch = input('What book(s) would you like to look for? (Name or author) ---> ')
-    foundItems = []
-    count = 0
-    for item in library:
-        if itemSearch.lower() in item.lower():
-            count += 1
-            foundItems.append(item)
-    if count == 0:
-        print('Couldn\'t find anything...')
-    elif count == 1:
-        print(f'The book {foundItems[0]} has been found in your library.')
-    else:
+    else: # Displays the itemS
         print('Here\'s a list of what we found in your library.')
         for i in range(1,count+1):
-            print(f'{i}. {foundItems[i-1]}')
+            print(f"{i}. {foundItems[i-1]['book']} by {foundItems[i-1]['author']}")
+        return foundItems
 
 
+def change(library):
+    book = search(library)
+    if book == False:
+        return
+    elif len(book) == 1:
+        pass
 
 # Contains my UI and options menu
 def main():
@@ -117,10 +123,11 @@ def main():
     while True:
         options = check_float(welcome_text + """What would you like to do?
 1. Display Contents
-2. Add Items
-3. Remove Items
-4. Search For Item
-5. Exit The Program
+2. Add a Book
+3. Remove a Book
+4. Search For a Book
+5. Change Book Information
+6. Exit The Program
    --->  """, 5)
         welcome_text = '\033c'
         print('\033c')
@@ -133,6 +140,8 @@ def main():
         elif options == 4:
             search(library)
         elif options == 5:
+            pass
+        elif options == 6:
             print('Thank you for using the library!')
             break
         else:
