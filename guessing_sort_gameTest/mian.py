7bc285a11dee458475332eb9ea746f944ce7039f
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-18 10:52:13 -0700

    Added thing cause why not

--- guessing_sort_gameTest/mian.py
+++ guessing_sort_gameTest/mian.py
@@ -1,21 +1,26 @@
-"""def sort_key(item):
-	
-	return(item)
+import math
+def sort_key(item):
+    score = 0
+    for letter in list(item):
+        order = ord(letter.lower())-96
+        if order > 13 and order <= 26:
+            score += math.sqrt(order)
+        if order <= 13 and order >= 1:
+            score -= math.cbrt(order)
+    score / len(item)
+    return(item)
 
 def user_word():
-	sentence = []
-    info = int(input('How many sentences would you like to write?'))
-    for i in range(info)
-        word = input("Write a sentence that could help you figure out my sorting algorithm")
+    sentence = []
+    info = int(input('How many words would you like to write? --->  '))
+    for i in range(info):
+        word = input("Write a word that could help you figure out my sorting algorithm. --->  ")
         sentence.append(word)
+    return sentence
 
 def main():
-	print('Welcome to my sorting guessing game! You just need to guess how I sorted your lists!')
-	user_word()
-	
-main()"""
-myDict = {'a':'abc','b':'abc','c':'dce','e':'fgh','g':'nop'}
-print(myDict)
-print(str(myDict))
-print(''.join(list(myDict.values())))
-print(myDict)
+    print('Welcome to my sorting guessing game! You just need to guess how I sorted your words!')
+    words = user_word()
+    words.sort(key=sort_key)
+    print(words)
+main()
