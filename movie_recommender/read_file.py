0dea05d6833fca83c685d17240cd0e188f2bac1c
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-02-26 00:41:04 -0700

    Added a little bit of whitespace

--- movie_recommender/read_file.py
+++ movie_recommender/read_file.py
@@ -1,5 +1,6 @@
 # This file reads the movie CSV file
 import csv
+
 def open_csv_column(column_name):
     with open("movie_recommender/Movies list.csv",'r',newline='') as movies:
         csvreader = csv.DictReader(movies, fieldnames=['Title','Director','Genre','Rating','Length (min)','Notable Actors'])
@@ -8,6 +9,7 @@ def open_csv_column(column_name):
         for row in csvreader:
             column_info.append(row[column_name])
     return column_info
+
 def open_csv_column_sep(column_name,separator):
     with open("movie_recommender/Movies list.csv",'r',newline='') as movies:
         csvreader = csv.DictReader(movies, fieldnames=['Title','Director','Genre','Rating','Length (min)','Notable Actors'])
@@ -17,6 +19,7 @@ def open_csv_column_sep(column_name,separator):
             column_info.append(row[column_name])
     column_info = [item for items in column_info for item in items.split(separator)]
     return column_info
+
 def open_csv_all():
     with open("movie_recommender/Movies list.csv",'r',newline='') as movies:
         csvreader = csv.reader(movies)
