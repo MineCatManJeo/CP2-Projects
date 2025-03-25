32365ad0d825f81ca48beb5fa7ba3852b529fa92
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-03-24 22:02:23 -0600

    Added battle file

--- battle_simulator/main.py
+++ battle_simulator/main.py
@@ -4,8 +4,6 @@ from InquirerPy import inquirer
 from read_file import read_file as read
 from write_file import write_file as write
 from select_char_menu import select_char_menu as sel_menu
-from select_char import select_char as sc
-from create_char import create_char as cc
 from skill_point import skills
 
 def main():
@@ -17,24 +15,6 @@ def main():
     selected_character = False
     spend_points = ""
 
-    # User character selection
-    ## User creates character
-    ### Name, blah blah, uses skill point function
-    ## User selects previosly made character
-    ## Variable used that is just the created / selected character
-
-    # menu for options to do with character
-    ## Select New Character
-    ## Delete Character
-    ## Add Skill points
-    ## Battle NPC / Possible other character (optional)
-    ## Check level, attributes, ect
-    ## Exit Program
-
-    # Variables:
-    # The whole list
-    # Names of each character in list maybe not, because we have everycharacter already
-    #### Once char is selected, specificly the character is a var
 
     while True:
         if not selected_character:
@@ -69,7 +49,6 @@ def main():
             break
             
         write('battle_simulator/storage_csvs/characters.csv',rf)
-        #inquirer.text(message="Press [Enter] to Continue").execute()
     print('\033cThank you for using my program!')
 
 
