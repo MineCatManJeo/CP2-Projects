24337564c0baeb0e1ed47b5b483c718f99f20555
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-03-31 10:46:43 -0600

    Added Compiter Change Careers Notes

---
+++ random_notes/cpter_cnge_crers_notes.py
@@ -0,0 +1,54 @@
+# What are some security concerns (for the user, not for the code) programmers need to think about when building a project? 
+
+#  Basic - Not needed but i want it in my notes
+#   Look nice
+#   Understand Easily
+#   Informative Errors
+#   Knowing what it does
+
+#  Security - Needed for notes
+#   Not asking for unnecessary things -
+#     Knowing what it does / what we need
+
+#   Info kept private -
+#     Encrypt User Info
+#     Passwords donverted to hash
+#     KEEP INFO SEPERATE
+
+
+# How do we take measures to address those security concerns? 
+
+#    We ensure we know what we need for the program
+#    We encrypt user info
+#    We convert passwords to hash
+#    WE KEEP INFO SEPERATE
+
+
+# What are some pro's and con's of the automation of jobs?
+
+#   Pros:
+#     Creating jobs for creating machine learning programs
+#     We can create machines that can do things too dangerous for humans
+#     Taking the jobs people don't want to do
+#     Less time spent doing harder jobs
+#     More quality products
+#     More quality of life in general because of new technology
+
+#   Cons:
+#     People lose many jobs, compared to population growth people less jobs are being formed
+#     Leads to more unemployeed and more impoverished people
+#     The select few people who own the machines might be the rulers of the world
+#     Quality of life may go down as more people become unemployed and poor.
+
+
+# What types of careers are available in computer science today?
+
+#   Machine Learning Engineer
+#   Hardware Egnineer
+#   Sofware Developer
+#   Cyber Security Engineer
+#   Technical Support Specialist
+#   IT Project Manager
+#   Cyber Operator
+#   Cryptographer
+#   Cloud Architect
